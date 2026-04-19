af94d1476a1cf9f3c040cc7476acae88496be5f3
shahheet09 <shahheet1911@gmail.com>
2026-04-19 19:30:49 +0530

    Add files via upload

---
+++ ALA_3_/Main.java
@@ -0,0 +1,6 @@
+public class Main { public static void main(String[] args) { 
+int n = 7; if(n % 2 == 0) 
+System.out.println("Even"); 
+else System.out.println("Odd"); 
+} 
+}
---
+++ ALA_3_/heet_ala_3.py
@@ -0,0 +1,8 @@
+# Even or Odd
+
+n = 7
+
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
